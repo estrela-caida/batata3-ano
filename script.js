02960e8c4b84c3da446ce6db6671a59d65ee9a78
estrela-caida <142031549+estrela-caida@users.noreply.github.com>
2025-03-24 08:42:42 -0300

    batata 9

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMcontentLoaded', function (){
     let tamanhoAtualFonte = 1;
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
 
+    aumentarFonteBotao.addEventListener('click', function(){ 
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem` 
+    })
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    diminuirFonteBotao.addEventListener('click', function(){ 
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem` 
+    })
 })
