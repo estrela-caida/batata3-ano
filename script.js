a8efb75c4bf9f61491f0e2477205f396be28321e
estrela-caida <142031549+estrela-caida@users.noreply.github.com>
2025-04-23 07:58:23 -0300

    batata

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMcontentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function (){
+    const botaoDeAcessibilidade =document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
     let tamanhoAtualFonte = 1;
 
     const aumentarFonteBotao = document.getElementById('aumentar-fonte');
